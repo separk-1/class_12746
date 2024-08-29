e031c565edffdef1484ec73fc6ef27f60e7f08a3
separk <separk@ses-macbook.wifi.local.cmu.edu>
2024-08-29 16:37:31 -0400

    0829_1637

--- day2_0829.py
+++ day2_0829.py
@@ -1,4 +1,19 @@
 import sys
+import math
 
 print("hello world")
-print(sys.version)
+print("python version: %s"%(sys.version))
+
+a = 3
+b = 2
+c = 2.3
+
+
+print("type of %s: %s"%(a, type(a)))
+print("type of %s: %s"%('a', type('a')))
+
+print("int(3.3) = %s"%(int(3.3)))
+print("bool(%s) = %s"%(a, bool(a)))
+
+print("sin %s = %s"%(a, math.sin(a)))
+print("%s ** %s = %s"%(a, b, a**b))
